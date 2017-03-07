358b7ebdc8b54f3020cb0996b73efa1ec0e99c67
skysun <739558011@qq.com>
2017-03-07 16:58:31 +0800

    同步github

---
+++ java-training/src/com/training/object/ObjectDate.java
@@ -0,0 +1,34 @@
+package com.training.object;
+
+import java.util.Date;
+
+public class ObjectDate {
+
+	private transient long fastTime;
+
+	// 无参构造方法
+	public ObjectDate() {
+
+		// 利用有参构造方法，初始化实例变量
+		this(System.currentTimeMillis());
+	}
+
+	public ObjectDate(Long time) {
+		fastTime = time;
+	}
+	
+	public long getTime(){
+		return fastTime;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		Date d = new Date();
+		
+		ObjectDate  dt = new ObjectDate();
+		
+		System.out.println(dt.getTime());
+		
+	}
+}

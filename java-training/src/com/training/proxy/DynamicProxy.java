9b00da7bd3f744c6d0e20eddc8335036513b34ab
skySun <739558011@qq.com>
2017-09-25 21:40:03 +0800

    提交代码

--- java-training/src/com/training/proxy/DynamicProxy.java
+++ java-training/src/com/training/proxy/DynamicProxy.java
@@ -4,7 +4,7 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 /**
- * 动态代理
+ * 动态代理(当我们得到一个对象,想动态为其一些方法每次被调用前后追加一些操作时,我们将会使用到动态代理)
  * 
  * @author skySun
  *
@@ -17,6 +17,7 @@ public class DynamicProxy implements InvocationHandler {
 		this.obj = obj;
 	}
 
+	
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 		System.out.println("before");
--- java-training/src/com/training/proxy/Main.java
+++ java-training/src/com/training/proxy/Main.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Proxy;
 
 public class Main {
 	public static void main(String[] args) {
+		
 		DynamicProxy inter = new DynamicProxy(new Vendor());
 
 		Sell sell = (Sell) Proxy.newProxyInstance(Sell.class.getClassLoader(), new Class[] { Sell.class }, inter);
